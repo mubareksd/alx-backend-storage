90a83774c1edf94a6c13933debadcd7df2ebe56d
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-05 22:31:57 +0300

    update

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,4 +1,5 @@
 -- Creates a trigger after order is made
-CREATE TRIGGER item_order AFTER INSERT ON `orders`
+DROP TRIGGER IF EXISTS `item_order`;
+CREATE TRIGGER IF NOT EXISTS item_order AFTER INSERT ON `orders`
 -- updates every row that an order has been made to quantity = quantity - order
 FOR EACH ROW UPDATE `items` SET quantity = quantity - New.number WHERE name=New.item_name;
