5a7bbc1732557e966513fad5ecdca413e603fb58
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-05 22:32:25 +0300

    solution to task 5, project 0x00-mysql_advanced

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Drop trigger validate_email if it exists
+DROP TRIGGER IF EXISTS `validate_email`;
+-- Creates trigger validate_email
+CREATE TRIGGER IF NOT EXISTS `validate_email` BEFORE UPDATE ON `users`
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+       SET NEW.valid_email = 0;
+    END IF;
+END;
