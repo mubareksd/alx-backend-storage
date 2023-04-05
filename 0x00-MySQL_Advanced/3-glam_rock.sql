89ecd3a2293d923ab0845c6723be3809164e2707
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-05 19:31:29 +0300

    update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,3 @@
 -- lists all bands with `Glam rock` as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(split, 2023) - formed) AS lifespan FROM `metal_bands` WHERE style LIKE "%Glam rock%";
+SELECT band_name, (IFNULL(split, 2023) - formed) AS lifespan
+FROM `metal_bands` WHERE style LIKE "%Glam rock%";
