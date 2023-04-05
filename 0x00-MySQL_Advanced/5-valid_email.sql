371eb711dd99915c66abdbc6b6d6c7ca8f9f7830
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-05 23:05:36 +0300

    update

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,11 @@
 -- Creates trigger validate_email
-DELIMITER $$ ;
+DELIMITER $$
 CREATE TRIGGER `validate_email` BEFORE UPDATE ON `users`
 FOR EACH ROW
 BEGIN
     IF NEW.email != OLD.email THEN
        SET NEW.valid_email = 0;
     END IF;
-END;$$
+END
+$$
 DELIMITER ;
