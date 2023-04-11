dbec4b012480b7e25a70557d817a4ba3eaf20e7d
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-11 12:59:47 +0300

    update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,18 +9,10 @@ if __name__ == "__main__":
     db = client.logs
     collection = db.nginx
     count = collection.count_documents({})
-    get_count = collection.count_documents({"method": "GET"})
-    post_count = collection.count_documents({"method": "POST"})
-    put_count = collection.count_documents({"method": "PUT"})
-    patch_count = collection.count_documents({"method": "PATCH"})
-    delete_count = collection.count_documents({"method": "DELETE"})
     status_check_count = collection.count_documents({"path": "/status"})
-
     print(f"{count} logs")
     print("Methods:")
-    print(f"    method GET: {get_count}")
-    print(f"    method POST: {post_count}")
-    print(f"    method PUT: {put_count}")
-    print(f"    method PATCH: {patch_count}")
-    print(f"    method DELETE: {delete_count}")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {count}")
     print(f"{status_check_count} status check")
