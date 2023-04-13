2492c739b941211972aecc7f3b87aa0a23038411
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-13 05:13:48 +0300

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,52 +1,43 @@
 #!/usr/bin/env python3
-"""web module
-"""
+""" web module """
+
 from functools import wraps
 from typing import Callable
+
 import redis
 import requests
+from requests import Response
+
+_redis = redis.Redis(host="localhost", port=6379, db=0)
 
 
-client = redis.Redis()
-
-
-def url_count(method: Callable) -> Callable:
-    """url_count function
-
-    Args:
-        method[Callable]:
-
-    returns:
-        Callable:
+def counter(method: Callable) -> Callable:
+    """
+    a counter decorator that counts how many times a particular URL was
+    accessed. The value is cached in Redis and will expire after 10 seconds
     """
 
     @wraps(method)
     def wrapper(*args, **kwargs):
-        """wrapper decorated function"""
-        url = args[0]
-        cache = client.get(f"cache:{url}")
-        if cache:
-            return cache.decode("utf-8")
-        client.incr(f"count:{url}")
-        client.setex(f"cache:{url}", 10, method(url))
-        return method(*args, **kwargs)
+        """
+        wrapper function
+        """
+        _redis.incr(f"count:{args[0]}")
+
+        html = _redis.get("html-cache:{args[0]}")
+        if html is not None:
+            return html.decode("utf-8")
+        html = method(*args, **kwargs)
+        _redis.setex(f"html-cache:{args[0]}", 10, html)
+        return html
 
     return wrapper
 
 
-@url_count
+@counter
 def get_page(url: str) -> str:
-    """get_page function
-
-    Args:
-        url[str]:
-
-    Returns:
-        str:
     """
-    response = requests.get(url)
+    a function that returns the HTML content of a particular URL
+    """
+    response: Response = requests.get(url)
     return response.text
-
-
-if __name__ == "__main__":
-    get_page("http://slowwly.robertomurray.co.uk")
