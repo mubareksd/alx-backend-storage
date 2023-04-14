e384644a558b04dfbb198c4ddf6f68cdac12443f
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-14 14:18:00 +0300

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,11 +8,11 @@ from requests import Response
 from typing import Callable
 
 
-client = redis.Redis()
+_redis = redis.Redis()
 
 
-def url_count(method: Callable) -> Callable:
-    """url_count function
+def count(method: Callable) -> Callable:
+    """count function
     Args:
         method[Callable]:
     returns:
@@ -23,18 +23,17 @@ def url_count(method: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         """wrapper decorated function"""
         url = args[0]
-        meth = method(*args, **kwargs)
-        client.incr("count:{}".format(url))
-        cached = client.get("cache:{}".format(url))
+        _redis.incr("count:{}".format(url))
+        cached = _redis.get("cache:{}".format(url))
         if cached:
             return cached.decode("utf-8")
-        client.setex("cache:{}".format(url), 10, meth)
-        return meth
+        _redis.setex("cache:{}".format(url), 10, method(url))
+        return method(*args, **kwargs)
 
     return wrapper
 
 
-@url_count
+@count
 def get_page(url: str) -> str:
     """get_page function
     Args:
@@ -44,3 +43,7 @@ def get_page(url: str) -> str:
     """
     response: Response = requests.get(url)
     return response.text
+
+
+if __name__ == "__main__":
+    get_page("http://google.com")
